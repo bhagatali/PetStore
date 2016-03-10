3a4e47e01f6db07f6f2b9c164c71af73b2595f7e
bhagatali <bhagat.ali@gmail.com>
2016-03-10 00:05:49 -0500

    Post Form Complete

--- src/main/resources/static/app/pet/pet.js
+++ src/main/resources/static/app/pet/pet.js
@@ -32,27 +32,51 @@ angular.module('myApp.pet',['ngRoute'])
       };
       
     $scope.submitAddPetForm = function(){
+    	//Convert String to Array
+    	var tags=$scope.tags.split(',');
+    	//New Array to hold JSON objects
+    	var tagArrayJSON = new Array();
+
+    	//Looping through the length of the array 
+    	for(var i=0;i<tags.length;i++){
+    		//Create a new JSON object
+    		var tagJson = new Object();
+    		//Add a tagname:tagValue 
+        	tagJson.tagName = tags[i];
+        	//Push the JSON object into JSON array
+        	tagArrayJSON.push(tagJson);
+    	}
+
+    	var categoryArrayJSON = new Array();
+
+    	for(var j=0;j<$scope.categories.length;j++){
+    		var categoryJson = new Object();
+    		categoryJson.categoryName = $scope.categories[j]; 
+    		categoryArrayJSON.push(categoryJson);
+    	}
+    	
     	var petObject={
     			petName:$scope.name || null,
     			status:$scope.status || null,
     			photoUrl:$scope.image || null,
-    			price:$scope.price || null
+    			price:$scope.price || null,
+    			tags:tagArrayJSON || null,
+    			categories:categoryArrayJSON || null
     	}
-    	console.log('petObject');
+    	console.log(petObject);
         var res = $http.post('http://localhost:8080/pet/',petObject);
     	
     	res.success(function(data, status, headers, config){
     		$scope.message = data;
+    		console.log($scope.message);
+    		$location.path('/pet/');
+    		
 		});
     	
 		res.error(function(data, status, headers, config) {
 			alert( "failure message: " + JSON.stringify({data: data}));
 		});
-//            var id = ref.key();
-//            console.log('Added record with id: ' + id);
-            
         clearFields();
-            
         $scope.addFormShowFlag = false;        
     };
     
@@ -61,6 +85,8 @@ angular.module('myApp.pet',['ngRoute'])
         $scope.status = "";
         $scope.image = "";
         $scope.price = "";
+        $scope.categories = "";
+        $scope.tags = "";
     };      
 }])
 .controller('PetDetailsController',['$scope',
